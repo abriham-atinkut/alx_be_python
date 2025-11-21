74f1469a6a57e735177298de65a330b4069ece72
unknown <abrihamatinkut@gmail.com>
2025-11-21 20:25:53 +0300

    function updata

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,7 +9,7 @@ if (temp_CF == "C" or temp_CF == "F") and isinstance(temp_value, int) :
             case "C":
                 def convert_to_fahrenheit(celsius):
                     global CELSIUS_TO_FAHRENHEIT_FACTOR
-                    CELSIUS_TO_FAHRENHEIT_FACTOR = (CELSIUS_TO_FAHRENHEIT_FACTOR*celsius)+32
+                    CELSIUS_TO_FAHRENHEIT_FACTOR = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
                     print(f"{celsius}°C is {CELSIUS_TO_FAHRENHEIT_FACTOR}°F")
                 convert_to_fahrenheit(temp_value)
             case "F":
@@ -22,5 +22,3 @@ else:
     print("Invalid temperature. Please enter a numeric value.")
 
 # git add . && git commit -m "function updata" && git push
-# CELSIUS_TO_FAHRENHEIT_FACTOR\s*\+\s*32 
-# CELSIUS_TO_FAHRENHEIT_FACTOR\s*\)\s*\+\s*32 - /tmp/correction/2465943663244944326984510408397459179239_5/100741/1508677/fns_and_dsa/temp_conversion_tool.py doesn't contain \(\s*CELSIUS_TO_FAHRENHEIT_FACTOR\s*\+\s*32\s*\) - /tmp/correction/2465943663244944326984510408397459179239_5/100741/1508677/fns_and_dsa/temp_conversion_tool.py doesn't contain 32\s*\+\s*\w+\s*\*\s*CELSIUS_TO_FAHRENHEIT_FACTOR
