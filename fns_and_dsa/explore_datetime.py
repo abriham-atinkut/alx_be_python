484c9d81319e892623982d147ae8ad99ed9182bb
unknown <abrihamatinkut@gmail.com>
2025-11-21 16:57:15 +0300

    updata

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
 from datetime import datetime, date, time, timedelta
 
 def display_current_datetime():
-    current_date = datetime.now()
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {current_date}")
 
 display_current_datetime()
@@ -13,7 +13,7 @@ def calculate_future_date():
 
     future_days_user_asks = timedelta(days=future_date)
     future_time = current_date + future_days_user_asks
-    formatted_date = future_time.strftime("%Y.%m.%d")
+    formatted_date = future_time.strftime("%Y-%m-%d")
     print(f"Future date: {formatted_date}")
 
 if __name__ == "__main__":
