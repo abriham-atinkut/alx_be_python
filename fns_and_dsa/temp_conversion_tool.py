42c51db7a1eb8b46988acce82bbc45d80cba34f4
unknown <abrihamatinkut@gmail.com>
2025-11-21 19:57:52 +0300

    updata temp conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,8 +3,8 @@
 temp_value = int(input("Enter the temperature to convert: "))
 temp_CF = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
 
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 
 match temp_CF:
     case "C":
